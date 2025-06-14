5a0d64a5870baaa97c376d45636e1f59ae21224b
i-niranjan <niranjanchaudhari2004@gmail.com>
2025-06-14 14:38:24 +0530

    Added Form Component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ import AddToDo from "./components/todo/AddTodo";
 
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import FeedbackForm from "./Project/FeedbackForm";
+const Card = ({ children }) => {
+  return <div className="p-4 border rounded-md shadow m-20">{children}</div>;
+};
+
+const BigHeading = ({ children }) => {
+  return (
+    <>
+      <h2 className="text-6xl text-black shadow-md">{children}</h2>
+    </>
+  );
+};
 export default function App() {
   return (
     <>
@@ -15,12 +27,19 @@ export default function App() {
       {/* <Counter />
       <PasswordGenerator /> */}
       {/* <CurrencyConverter /> */}
-      <Provider store={store}>
+      {/* <Provider store={store}>
         <div className="flex flex-col w-full h-[100vh] justify-center items-center">
           <AddToDo />
           <ToDo />
         </div>
-      </Provider>
+      </Provider> */}
+      {/* <Card>
+        <h1>Hello from inside the Card!</h1>
+        <p>This is some more content.</p>
+      </Card>
+      <BigHeading>Hello</BigHeading> */}
+
+      <FeedbackForm />
     </>
   );
 }
---
+++ src/Project/FeedbackForm.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+function FeedbackForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  const firstName = watch("firstName");
+  console.log(firstName);
+
+  return (
+    <div className="h-[100vh] flex justify-center items-center  w-full bg-slate-400">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label
+              htmlFor="first_name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              <p>Your name is: {firstName}</p>
+            </label>
+            <input
+              {...register("firstName")}
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="John"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="last_name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Last name
+            </label>
+            <input
+              {...register("lastname")}
+              type="text"
+              id="last_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Doe"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="company"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Company
+            </label>
+            <input
+              {...register("company")}
+              type="text"
+              id="company"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Flowbite"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Phone number
+            </label>
+            <input
+              {...register("number")}
+              type="tel"
+              id="phone"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="123-45-678"
+              required
+            />
+          </div>
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email address
+          </label>
+          {errors.email && <p>Email is required</p>}
+          <input
+            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="john.doe@company.com"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            {...register("password")}
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="•••••••••"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="confirm_password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Confirm password
+          </label>
+          <input
+            {...register("confirmpass")}
+            type="password"
+            id="confirm_password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="•••••••••"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default FeedbackForm;
